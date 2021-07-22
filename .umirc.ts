5ed17fe491b26c3a6a681490d583bd21e9cf9bc5
linzhenfang <linzhenfang@pinming.cn>
2021-07-22 10:06:18 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,10 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  publicPath: '/',
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  headScripts: [
+    'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-language/v0.10.0/mapbox-gl-language.js',
+  ],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import MapboxLanguage from '@mapbox/mapbox-gl-language';
 
 const styles = {
   width: '100vw',
@@ -15,21 +14,17 @@ const MapboxGLMap = () => {
 
   useEffect(() => {
     mapboxgl.accessToken =
-      'pk.eyJ1IjoicGlrZW1hbiIsImEiOiJja2R2ZmR3cjkwZmJpMzBvc2MzanRtZ3kzIn0.f5jHc6Y_3xUfKqzxTW1Fyg';
+      'pk.eyJ1IjoicGlrZW1hbiIsImEiOiJja2RzZ29sbnkwOXh4MzRuMjZ5NDhiN3V6In0.CzfkUypNYwZT0YHvwMsVag';
     const initializeMap = ({ setMap, mapContainer }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
-        style: 'mapbox://styles/pikeman/ckrda36i31aon17ocldrx0hj9', // stylesheet location
+        style: 'mapbox://styles/pikeman/ckrda36i31aon17ocldrx0hj9',
         center: [119.19, 29.88],
         zoom: 10,
         maxZoom: 16,
-        minZoom: 6,
+        minZoom: 8,
         maxPitch: 60,
       });
-      var language = new MapboxLanguage({
-        defaultLanguage: 'zh',
-      });
-      map.addControl(language);
 
       map.on('load', () => {
         setMap(map);
