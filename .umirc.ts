2a33505c87271d571475d9a45b33b12294a4f868
linzhenfang <linzhenfang@pinming.cn>
2021-07-22 10:43:38 +0800

    webpack5

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   publicPath: '/',
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  webpack5: {},
   headScripts: [
     'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-language/v0.10.0/mapbox-gl-language.js',
   ],
